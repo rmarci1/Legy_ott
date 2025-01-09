61888396fd89445b97e5bae4199ee7f2c4e4de88
Marcell Rabotka <rigbimarci@gmail.com>
2025-01-09 12:58:47 +0100

    starting state

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import "../global.css"
+const index = () => {
+  return (
+    <View>
+      <Text className='text-3xl'>index</Text>
+    </View>
+  )
+}
+
+export default index
